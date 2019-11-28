cb1052da030891e9ba66d40896e0b7f307fbaf61
slim <husseinbja@yahoo.de>
2019-11-28 13:55:52 +0100

    Update DatesImprove.java

--- DatesImprove.java
+++ DatesImprove.java
@@ -1,8 +1,13 @@
 public static int daysInMounth(int month){
-	if((mounth==9)|| (month==4)||(month==6)||(month==11)){
-	return 30;
-	}else if (mounth==2){
-	return 28 ;
-	}else{retrun 31;}
-
+	if (month >=1 && month <= 12 ){
+		if((month==9)||(month==4)||(month==6)||(month==11)){
+			return 30;
+		}else if(mounth==2){
+			return 28;
+		}else{
+			return 31;
+		}
+		}else{
+			return null;
+		}
 }
